47a342df157cf9b3e3993620c6217c877dd04448
PauloPalauro <paulopalauro@gmail.com>
2024-02-21 15:59:06 -0300

    Obtendo um usuário por id e excessões

--- src/main/java/com/palauro/workshopmongo/resources/UserResource.java
+++ src/main/java/com/palauro/workshopmongo/resources/UserResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +27,13 @@ public class UserResource {
         List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList()); // Convertendo cada item da lista original de user em userDTO. 
         return ResponseEntity.ok().body(listDto);
     }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET) // Saber o metodo HTTP que será usado nesse endpoint.
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) { // Retornando uma list de UserDTO.
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(obj)); //Convertendo para DTO.
+    }
+
+
 
 }
--- src/main/java/com/palauro/workshopmongo/services/UserService.java
+++ src/main/java/com/palauro/workshopmongo/services/UserService.java
@@ -1,10 +1,13 @@
 package com.palauro.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.palauro.workshopmongo.domain.User;
 import com.palauro.workshopmongo.repository.UserRepository;
+import com.palauro.workshopmongo.services.exception.ObjectNotFoundException;
 
 // Camada de Serviço
 
@@ -14,8 +17,12 @@ public class UserService {
     @Autowired
     private UserRepository repository;
 
-    public List<User> findALl(){
+    public List<User> findALl() {
         return repository.findAll();
     }
-    
+
+    public User findById(String id){
+        Optional<User> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
 }
